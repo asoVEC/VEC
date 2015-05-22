b4a9ebf256e83a3594190651a10590beb0daf71d
Akihiro <Aki@Aki.local>
2015-05-22 15:19:38 +0900

    2015/05/22

--- model.php
+++ model.php
@@ -9,17 +9,17 @@ $port = 8889;
 // db接続
 function open_database_connection()
 {
-	$link = mysql_connect("$host:$port", $user, $password);
-	if (!$link) {
-		die('接続失敗です。'.mysql_error());
-	}
+$link = mysql_connect("$host:$port", $user, $password);
+if (!$link) {
+    die('接続失敗です。'.mysql_error());
+}
 
-	$db_selected = mysql_select_db('vec',$link);
-	if (!$db_selected){
-		die('データベース選択失敗です。'.mysql_error());
-	}
+$db_selected = mysql_select_db($db,$link);
+if (!$db_selected){
+    die('データベース選択失敗です。'.mysql_error());
+}
 
-	return $link;
+return $link;
 }
 // db解除
 function close_database_connection($link)
@@ -32,23 +32,24 @@ function get_user_name()
 	$link = open_database_connection();
 	$result = mysql_query('SELECT name FROM user', $link);
 	if (!$result) {
-		die('クエリーが失敗しました。'.mysql_error());
-	}
+    die('クエリーが失敗しました。'.mysql_error());
+}
 	$name = mysql_fetch_assoc($result);
 
-	close_database_connection($link);
-	return $name;
+    close_database_connection($link);
+    return $name;
 }
 // メールアドレス取得
 function get_user_mailaddress()
 {
 	$link = open_database_connection();
-	$result = mysql_query('SELECT address FROM user', $link);
+	echo "string";
+	$result = mysql_query('SELECT address FROM product', $link);
 	$mailaddress = mysql_fetch_assoc($result);
 
-	close_database_connection($link);
+    close_database_connection($link);
 
-	return $mailaddress;
+    return $mailaddress;
 }
 echo "aa";
 $name = get_user_name();
