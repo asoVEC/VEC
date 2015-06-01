c4f152c4cf66d2a1c3d4f10940d57c5e449ebd7b
AkihiroYasunaga <Aki@Aki.local>
2015-06-01 16:06:01 +0900

    --

---
+++ test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+require "Model/BaseModel.php";
+$user     = 'root';
+$password = 'root';
+$db       = 'vec';
+$host     = 'localhost';
+$port     = 8889;
+
+$link = mysql_connect(
+	"$host:$port",
+	$user,
+	$password
+);
+if (!$link) {
+	die('接続失敗です。'.mysql_error());
+}
+$db_selected = mysql_select_db($db, $link);
+if (!$db_selected) {
+	die('データベース選択失敗です。'.mysql_error());
+}
+$result = mysql_query('SELECT * FROM user');
+if (!$result) {
+	die('クエリーが失敗しました。'.mysql_error());
+}
+$row = mysql_fetch_assoc($result);
+print($row['address']);
+
+?>
