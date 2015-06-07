9758b323ad630abf0fc396c00c095a0487715fb3
Aki <lbycnxrrxn765v2+github@gmail.com>
2015-06-08 04:34:27 +0900

    #10 User2,BaseModel, temp.php

--- View/temp.php
+++ View/temp.php
@@ -1,11 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+</title>            
+</head>
+<body>
+
+</body>
+</html>
 <?php
-$user     = 'root';
-$password = 'root';
-$db       = 'vec';
-$host     = 'localhost';
-$port     = 8889;
+require_once '../Model/BaseModel.php';
+require_once '../Model/User2.php';
 
-$dsn = 'mysql:host=localhost;dbname=vec;port=8889;';
-$pdo = new PDO($dsn, $user, $password);
 
-?>
+$obj = new User2();
+echo('<br />name: '.$obj->getUser()['name']);
+echo('<br />address: '.$obj->getUser()['address']);
+?> 
