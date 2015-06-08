f739ef3caff16b780b3d2588d048a08a7c3922a0
Aki <lbycnxrrxn765v2+github@gmail.com>
2015-06-08 16:00:16 +0900

    Userファイル名変更とか

--- Model/User.php
+++ Model/User.php
@@ -1,59 +1,24 @@
-//Model User
 <?php
-// 変数
-$user = 'root';
-$password = 'root';
-$db = 'vec';
-$socket = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock';
-// db接続
-function open_database_connection()
-{
-$link = mysql_connect(
-   $socket,
-   $user,
-   $password
-);
-if (!$link) {
-    die('接続失敗です。'.mysql_error());
-}
+class User extends BaseModel{
+    function __construct() {
+        parent::__construct();
+    }
+    
+    
+    function getUser() {
+        $Row = parent::query('user',0);
+        return $Row;
+    }
+    function login($mailAddress,$password){
+        $where = 'mail_address = '.$mailAddress;
+        $row = parent::query('user', $where);
+        if ($row['password'] == $password){
+            return 1;
+        }  else {
+            return 0;  
+        }
+    }
 
-$db_selected = mysql_select_db(
-   'test',
-   $link
-);
-if (!$db_selected){
-    die('データベース選択失敗です。'.mysql_error());
-}
-return $link;
-}
-// db解除
-function close_database_connection($link)
-{
-	mysql_close($link);
-}
-
-// 名前取得
-function get_user_name()
-{
-	$link = open_database_connection();
-	$result = mysql_query('SELECT test FROM test', $link);
-	if (!$result) {
-    die('クエリーが失敗しました。'.mysql_error());
-}
-	$name = mysql_fetch_assoc($result);
-
-    close_database_connection($link);
-    return $name;
-}
-// メールアドレス取得
-function get_user_mailaddress()
-{
-	$link = open_database_connection();
-	$result = mysql_query('SELECT address FROM user', $link);
-	$mailaddress = mysql_fetch_assoc($result);
-
-    close_database_connection($link);
-
-    return $mailaddress;
 }
 ?>
+
--- View/temp.php
+++ View/temp.php
@@ -10,11 +10,11 @@
 </html>
 <?php
 require_once '../Model/BaseModel.php';
-require_once '../Model/User2.php';
+require_once '../Model/User.php';
 
 
 
-$obj = new User2();
+$obj = new User();
 echo('<br />name: '.$obj->getUser()['name']);
 echo('<br />address: '.$obj->getUser()['address']);
 echo ('<br />'.$obj->login('000@gmail.com', '001'));
