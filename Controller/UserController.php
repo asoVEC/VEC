bae2673c99551b29555c85a6b4d12fa72fba307e
Shohe Masuyama <cisuco00@gmail.com>
2015-06-23 14:35:04 +0900

    tt
    ttt

---
+++ Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+class userController {
+    
+    private $view;    
+    public function __construct()
+    {
+        require_once('smarty/libs/Smarty.class.php');
+//        require_once 'post.php';
+        // ビュー
+        $this->view = new Smarty;
+//        $this->view->template_dir = '../View';
+    }
+    
+    
+    function setting(){
+        $this->view->display('View/user-settings.tpl'); 
+    }
+    
+    
+}
+
+?>
