5040e02fb682c95fd659314fa1645468400e8a5a
takahironagano0205 <exile.taka-0205@docomo.ne.jp>
2015-06-23 15:07:01 +0900

    .

---
+++ templates_c/61f29f12c5ca8e6327f1a9043ce8e7490103809a_0.file.base.tpl.php
@@ -0,0 +1,137 @@
+<?php /* Smarty version 3.1.24, created on 2015-06-23 07:18:27
+         compiled from "F:/xampp/htdocs/VEC/View/base.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:190945588ec23c31190_62151895%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '61f29f12c5ca8e6327f1a9043ce8e7490103809a' => 
+    array (
+      0 => 'F:/xampp/htdocs/VEC/View/base.tpl',
+      1 => 1434956046,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '190945588ec23c31190_62151895',
+  'variables' => 
+  array (
+    'account_info' => 0,
+    'amount_cart' => 0,
+    'name1' => 0,
+    'food1' => 0,
+    'name2' => 0,
+    'food2' => 0,
+    'name3' => 0,
+    'food3' => 0,
+    'name4' => 0,
+    'food4' => 0,
+    'name5' => 0,
+    'food5' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => '3.1.24',
+  'unifunc' => 'content_5588ec23d61c99_56611250',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_5588ec23d61c99_56611250')) {
+function content_5588ec23d61c99_56611250 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '190945588ec23c31190_62151895';
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ホームページ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/VEC/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="/VEC/css/css.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <a href="#" rel="tooltip" data-toggle="tooltip" data-placement="bottom" title="this is tooltip">tooltip</a>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-header">
+
+                <!--ブランド名 ロゴ名の表示-->
+                <a class="navbar-brand" href="#">VEC</a>
+            </div>
+            <div id="nav-content" class="collapse navbar-collapse">
+                <!--                    リンクのリスト メニューリスト-->
+                <div class="navbar-right">
+                <ul class="nav navbar-nav">
+                    <li><a href="/VEC/Controller/home/login">アカウントサービス<?php echo $_smarty_tpl->tpl_vars['account_info']->value;?>
+</a></li>
+                    <li><a href="">買い物カゴ <span class="badge"><?php echo $_smarty_tpl->tpl_vars['amount_cart']->value;?>
+</span></a></li>
+                </ul>
+            </div>
+                <!--検索フォーム-->
+                <form class="navbar-form navbar-static-top" role="search">
+                    <div class="form-group col-md-offset-1">
+                        <input type="text" class="form-control" placeholder="商品検索" size="60pix">
+                    </div>
+                    <button type="submit" class="btn btn-default">検索</button>
+                </form>
+            </div>
+        </nav>
+        <header class="jumbotron col-lg-10 col-lg-offset-1"></header>
+
+      <form class="navbar-form navbar-static-top hidden-lg hidden-md hidden-sm" role="search">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="商品検索">
+            </div>
+            <button type="submit" class="btn btn-default">検索</button>
+        </form>
+
+        <div class="container main-content">
+            <div class="row">
+                <div class="col-md-3 col-lg-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li class="active"><a href="#">カテゴリ一覧</a></li>
+                        <li><a href="#">肉・卵</a></li>
+                        <li><a href="#">魚介</a></li>
+                        <li><a href="#">野菜</a></li>
+                        <li><a href="#">果物</a></li>
+                        <li><a href="#">穀物</a></li>
+                        <li><a href="#">調味料</a></li>
+                        <li><a href="#">加工食品</a></li>
+                        <li><a href="#">菓子</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-9 content-area">
+                    <style type="text/css">
+                        
+                        
+                    </style>
+                    <div class="label"><?php echo $_smarty_tpl->tpl_vars['name1']->value;?>
+</div>
+                    <div class="frame" style="background: <?php echo $_smarty_tpl->tpl_vars['food1']->value;?>
+ center center no-repeat;"></div>
+                    <div class="label"><?php echo $_smarty_tpl->tpl_vars['name2']->value;?>
+</div>
+                    <div class="frame" style="background: <?php echo $_smarty_tpl->tpl_vars['food2']->value;?>
+ center center no-repeat;"></div>
+                    <div class="label"><?php echo $_smarty_tpl->tpl_vars['name3']->value;?>
+</div>
+                    <div class="frame" style="background: <?php echo $_smarty_tpl->tpl_vars['food3']->value;?>
+ center center no-repeat;"></div>
+                    <div class="label"><?php echo $_smarty_tpl->tpl_vars['name4']->value;?>
+</div>
+                    <div class="frame" style="background: <?php echo $_smarty_tpl->tpl_vars['food4']->value;?>
+ center center no-repeat;"></div>
+                    <div class="label"><?php echo $_smarty_tpl->tpl_vars['name5']->value;?>
+</div>
+                    <div class="frame" style="background: <?php echo $_smarty_tpl->tpl_vars['food5']->value;?>
+ center center no-repeat;"></div>
+                </div>
+            </div>
+        </div>
+        <footer class="container-fluid">
+            <small><a href="/">Copyright (C) 2013-2014 9ineBB All Rights Reserved.</a></small>
+        </footer>
+    </body>
+</html>
+
+<?php }
+}
+?>
