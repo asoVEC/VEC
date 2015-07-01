b1e9154a02f14c4103ed23910b04e6e4276e316c
Aki <lbycnxrrxn765v2+github@gmail.com>
2015-07-01 11:43:56 +0900

    .

--- Model/Product.php
+++ Model/Product.php
@@ -1,43 +1,32 @@
 <?php
+
 require_once 'BaseModel.php';
-class User extends BaseModel {
+
+class Prodct extends BaseModel {
+
     private $productNo;
     private $productName;
-//    private $mail;
-//    private $age;
-//    private $gender;
-//    private $address;
-//    private $point;
-//    private $credit;
-//    private $money;
 
     function __construct() {
         parent::__construct();
+    }
+
+    function __destruct() {
+        parent::__destruct();
     }
 
     function getProductName() {
         return $this->productName;
     }
 
-//    function getUserNo() {
-//        return $this->userNo;
-//    }
-//
-//    function getUser() {
-//        $Row = parent::query('user', 0);
-//        return $Row;
-//    }
-    
     //商品検索
-    
     //カテゴリ別一覧
-    function byCategory($category){
-        $where = 'category_no='.'\''.$category.'\'';
+    function byCategory($category) {
+        $where = 'category_no=' . '\'' . $category . '\'';
     }
-    
-    
+
     function login($mailAddress, $password) {
-        $where = 'mail_address = ' . '\''.$mailAddress.'\''; //←検索条件のとこ''忘れないようにね!
+        $where = 'mail_address = ' . '\'' . $mailAddress . '\''; //←検索条件のとこ''忘れないようにね!
         $row = parent::query('user', $where);
         if ($row['password'] === $password) {   //←==比較すると '0'=='000'が通るので注意!
             $this->userNo = $row['user_no'];
@@ -48,6 +37,8 @@ class User extends BaseModel {
         }
     }
 
-    function signUp() {   
+    function signUp() {
+        
     }
+
 }
